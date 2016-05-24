a210c67e2e661d9ae32621a984c52e796918dde9
André Ribeiro <andre.ribeiro@ist.utl.pt>
2016-05-24 12:56:44 +0100

    Started JSON gen implementation

--- rslingo.rslil/src/rslingo/rslil/generator/RSLIL2JsonGenerator.xtend
+++ rslingo.rslil/src/rslingo/rslil/generator/RSLIL2JsonGenerator.xtend
@@ -4,6 +4,10 @@ import org.eclipse.emf.ecore.resource.Resource
 import org.eclipse.xtext.generator.IFileSystemAccess
 import org.eclipse.xtext.generator.IGenerator
 import rslingo.rslil.rSLIL.PackageProject
+import rslingo.rslil.rSLIL.GlossaryTerm
+import rslingo.rslil.rSLIL.Stakeholder
+import rslingo.rslil.rSLIL.Goal
+import rslingo.rslil.rSLIL.PackageSystem
 
 class RSLIL2JsonGenerator implements IGenerator {
 
@@ -21,7 +25,31 @@ class RSLIL2JsonGenerator implements IGenerator {
 	def compile(PackageProject packageProject)
 	'''{
 		"name": "«packageProject.project.name»",
-		
+		"description": "«packageProject.project.description»",
+		«IF !packageProject.glossaryTerms.empty»"Glossary": [
+	    «FOR g:packageProject.glossaryTerms SEPARATOR ','»«g.compile»«ENDFOR»],«ENDIF»
+	    «IF !packageProject.stakeholders.empty»"Stakeholders": [
+	    «FOR s:packageProject.stakeholders SEPARATOR ','»«s.compile»«ENDFOR»],«ENDIF»
+	    «IF !packageProject.goals.empty»"Goals": [
+	    «FOR g:packageProject.goals SEPARATOR ','»«g.compile»«ENDFOR»],«ENDIF»
+	    «IF !packageProject.packageSystems.empty»"Systems": [
+	    «FOR s:packageProject.packageSystems SEPARATOR ','»«s.compile»«ENDFOR»],«ENDIF»
 	}
+	'''
+	
+	def compile(GlossaryTerm g)
+	'''
+	'''
+	
+	def compile(Stakeholder s)
+	'''
+	'''
+	
+	def compile(Goal g)
+	'''
+	'''
+	
+	def compile(PackageSystem g)
+	'''
 	'''
 }
--- rslingo.rslil/xtend-gen/rslingo/rslil/generator/RSLIL2JsonGenerator.java
+++ rslingo.rslil/xtend-gen/rslingo/rslil/generator/RSLIL2JsonGenerator.java
@@ -2,6 +2,7 @@ package rslingo.rslil.generator;
 
 import com.google.common.collect.Iterators;
 import java.util.Iterator;
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
@@ -11,8 +12,12 @@ import org.eclipse.xtext.generator.IFileSystemAccess;
 import org.eclipse.xtext.generator.IGenerator;
 import org.eclipse.xtext.xbase.lib.Functions.Function1;
 import org.eclipse.xtext.xbase.lib.IteratorExtensions;
+import rslingo.rslil.rSLIL.GlossaryTerm;
+import rslingo.rslil.rSLIL.Goal;
 import rslingo.rslil.rSLIL.PackageProject;
+import rslingo.rslil.rSLIL.PackageSystem;
 import rslingo.rslil.rSLIL.Project;
+import rslingo.rslil.rSLIL.Stakeholder;
 
 @SuppressWarnings("all")
 public class RSLIL2JsonGenerator implements IGenerator {
@@ -49,10 +54,139 @@ public class RSLIL2JsonGenerator implements IGenerator {
     _builder.append("\",");
     _builder.newLineIfNotEmpty();
     _builder.append("\t\t");
-    _builder.newLine();
+    _builder.append("\"description\": \"");
+    Project _project_1 = packageProject.getProject();
+    String _description = _project_1.getDescription();
+    _builder.append(_description, "\t\t");
+    _builder.append("\",");
+    _builder.newLineIfNotEmpty();
+    _builder.append("\t\t");
+    {
+      EList<GlossaryTerm> _glossaryTerms = packageProject.getGlossaryTerms();
+      boolean _isEmpty = _glossaryTerms.isEmpty();
+      boolean _not = (!_isEmpty);
+      if (_not) {
+        _builder.append("\"Glossary\": [");
+        _builder.newLineIfNotEmpty();
+        _builder.append("\t    ");
+        {
+          EList<GlossaryTerm> _glossaryTerms_1 = packageProject.getGlossaryTerms();
+          boolean _hasElements = false;
+          for(final GlossaryTerm g : _glossaryTerms_1) {
+            if (!_hasElements) {
+              _hasElements = true;
+            } else {
+              _builder.appendImmediate(",", "\t    ");
+            }
+            CharSequence _compile = this.compile(g);
+            _builder.append(_compile, "\t    ");
+          }
+        }
+        _builder.append("],");
+      }
+    }
+    _builder.newLineIfNotEmpty();
+    _builder.append("\t    ");
+    {
+      EList<Stakeholder> _stakeholders = packageProject.getStakeholders();
+      boolean _isEmpty_1 = _stakeholders.isEmpty();
+      boolean _not_1 = (!_isEmpty_1);
+      if (_not_1) {
+        _builder.append("\"Stakeholders\": [");
+        _builder.newLineIfNotEmpty();
+        _builder.append("\t    ");
+        {
+          EList<Stakeholder> _stakeholders_1 = packageProject.getStakeholders();
+          boolean _hasElements_1 = false;
+          for(final Stakeholder s : _stakeholders_1) {
+            if (!_hasElements_1) {
+              _hasElements_1 = true;
+            } else {
+              _builder.appendImmediate(",", "\t    ");
+            }
+            CharSequence _compile_1 = this.compile(s);
+            _builder.append(_compile_1, "\t    ");
+          }
+        }
+        _builder.append("],");
+      }
+    }
+    _builder.newLineIfNotEmpty();
+    _builder.append("\t    ");
+    {
+      EList<Goal> _goals = packageProject.getGoals();
+      boolean _isEmpty_2 = _goals.isEmpty();
+      boolean _not_2 = (!_isEmpty_2);
+      if (_not_2) {
+        _builder.append("\"Goals\": [");
+        _builder.newLineIfNotEmpty();
+        _builder.append("\t    ");
+        {
+          EList<Goal> _goals_1 = packageProject.getGoals();
+          boolean _hasElements_2 = false;
+          for(final Goal g_1 : _goals_1) {
+            if (!_hasElements_2) {
+              _hasElements_2 = true;
+            } else {
+              _builder.appendImmediate(",", "\t    ");
+            }
+            CharSequence _compile_2 = this.compile(g_1);
+            _builder.append(_compile_2, "\t    ");
+          }
+        }
+        _builder.append("],");
+      }
+    }
+    _builder.newLineIfNotEmpty();
+    _builder.append("\t    ");
+    {
+      EList<PackageSystem> _packageSystems = packageProject.getPackageSystems();
+      boolean _isEmpty_3 = _packageSystems.isEmpty();
+      boolean _not_3 = (!_isEmpty_3);
+      if (_not_3) {
+        _builder.append("\"Systems\": [");
+        _builder.newLineIfNotEmpty();
+        _builder.append("\t    ");
+        {
+          EList<PackageSystem> _packageSystems_1 = packageProject.getPackageSystems();
+          boolean _hasElements_3 = false;
+          for(final PackageSystem s_1 : _packageSystems_1) {
+            if (!_hasElements_3) {
+              _hasElements_3 = true;
+            } else {
+              _builder.appendImmediate(",", "\t    ");
+            }
+            CharSequence _compile_3 = this.compile(s_1);
+            _builder.append(_compile_3, "\t    ");
+          }
+        }
+        _builder.append("],");
+      }
+    }
+    _builder.newLineIfNotEmpty();
     _builder.append("\t");
     _builder.append("}");
     _builder.newLine();
+    return _builder;
+  }
+  
+  public CharSequence compile(final GlossaryTerm g) {
+    StringConcatenation _builder = new StringConcatenation();
+    return _builder;
+  }
+  
+  public CharSequence compile(final Stakeholder s) {
+    StringConcatenation _builder = new StringConcatenation();
+    return _builder;
+  }
+  
+  public CharSequence compile(final Goal g) {
+    StringConcatenation _builder = new StringConcatenation();
+    return _builder;
+  }
+  
+  public CharSequence compile(final PackageSystem g) {
+    StringConcatenation _builder = new StringConcatenation();
     return _builder;
   }
 }
